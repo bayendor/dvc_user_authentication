4111e3a99425dca8963d4141775ce7f7903b6590
David Bayendor <dbayendor@gmail.com>
2013-11-27 18:29:32 -0700

    Refactor login authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,19 @@
 class SessionsController < ApplicationController
-
   def login
   end
 
   def create
-    if params[:login][:email].present? &&
-       params[:login][:password].present?
     @user = User.find_by_email(params[:login][:email])
-
-      if @user.nil?
-        redirect_to :login, notice: "Invalid email/password combination"
-      else
-        if @user.authenticate(params[:login][:password])
-          session[:user_id] = @user.id
-          redirect_to root_path, notice: "Logged in sucessfully"
-        else redirect_to :login, notice: "Invalid email/password combination"
-        end
-      end
+    if @user && @user.authenticate(params[:login][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Logged in sucessfully'
     else
-      redirect_to :login, notice: "Invalid email/password combination"
+      redirect_to :login, notice: 'Invalid email/password combination'
     end
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to root_path, notice: "You've been logged out."
+    redirect_to root_path, success: 'Successfully logged out.'
   end
-
 end
